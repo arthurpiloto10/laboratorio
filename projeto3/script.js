d70fd09f94f08f5a37a1f65e82bb5d678627e836
arthurpiloto10 <arthursccp76@gmail.com>
2025-05-21 18:59:50 -0300

    Criando a página do carrinho

--- projeto3/script.js
+++ projeto3/script.js
@@ -18,41 +18,48 @@
 //     }
 // });
 
-document.getElementById('hamburguer').addEventListener('click', () => {
-    document.getElementById('menu').classList.toggle('d-none');
+document.getElementById("hamburguer").addEventListener("click", () => {
+  document.getElementById("menu").classList.toggle("d-none");
 });
 
-document.getElementById('sub-menu').addEventListener('click', () => {
-    document.getElementById('menu-categoria').classList.toggle('d-none');
+document.getElementById("sub-menu").addEventListener("click", () => {
+  document.getElementById("menu-categoria").classList.toggle("d-none");
 });
 
 function estaVisivel(elemento) {
-    const posicao = elemento.getBoundingClientRect();
-    return posicao.top <= window.innerHeight && posicao.bottom >= 0;
+  const posicao = elemento.getBoundingClientRect();
+  return posicao.top <= window.innerHeight && posicao.bottom >= 0;
 }
 
 function aplicarAnimacoes() {
-    const elementos = document.querySelectorAll(".fade-up, .fade-down, .fade-left, .fade-right");
-    elementos.forEach((elemento) => {
-        if(estaVisivel(elemento) && !elemento.classList.contains('visible')) {
-            const delay = elemento.dataset.delay || '0s';
-            elemento.style.transitionDelay = delay;
-            elemento.classList.add('visible');
-        }
-    })
+  const elementos = document.querySelectorAll(
+    ".fade-up, .fade-down, .fade-left, .fade-right"
+  );
+  elementos.forEach((elemento) => {
+    if (estaVisivel(elemento) && !elemento.classList.contains("visible")) {
+      const delay = elemento.dataset.delay || "0s";
+      elemento.style.transitionDelay = delay;
+      elemento.classList.add("visible");
+    }
+  });
 }
 
-document.addEventListener('DOMContentLoaded', aplicarAnimacoes);
-window.addEventListener('scroll', aplicarAnimacoes);
+document.addEventListener("DOMContentLoaded", aplicarAnimacoes);
+window.addEventListener("scroll", aplicarAnimacoes);
 
-window.fetch('http://127.0.0.1:5500/projeto3/produtos.json').then(resposta => resposta.json()).then(produtos => {
-    const listaProdutos = document.getElementById('lista-produtos');
+window
+  .fetch("http://127.0.0.1:5500/projeto3/produtos.json")
+  .then((resposta) => resposta.json())
+  .then((produtos) => {
+    const listaProdutos = document.getElementById("lista-produtos");
     let html = ``;
-    produtos.forEach(produto => {
-        html += `
+    produtos.forEach((produto) => {
+      html += `
         <div class="card p-2 w-lg-25 fade-left">
                 <div class="imagem d-flex align-items-center justify-content-center">
-                    <img src="./img/${produto.imagem}" alt="${produto.nome}" class="img-fluid">
+                    <img src="./img/${produto.imagem}" alt="${
+        produto.nome
+      }" class="img-fluid">
                 </div>
                 <h3 class="text-uppercase mt-1">
                     ${produto.nome}
@@ -64,7 +71,7 @@ window.fetch('http://127.0.0.1:5500/projeto3/produtos.json').then(resposta => re
                     <span>
                         R$
                         <span class="text-success">
-                            ${produto.preco.toFixed(2).replace('.',',')}
+                            ${produto.preco.toFixed(2).replace(".", ",")}
                         </span>
                     </span>
                     <a href="" class="btn1">
@@ -75,4 +82,17 @@ window.fetch('http://127.0.0.1:5500/projeto3/produtos.json').then(resposta => re
         `;
     });
     listaProdutos.innerHTML = html;
-})
+  });
+
+function adicionarProdutoAoCarrinho(p_id, p_nome, p_imagem, p_preco) {
+  let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+  let novoProduto = {
+    id: p_id,
+    nome: p_nome,
+    imagem: p_imagem,
+    preco: p_preco,
+    quantidade: 1,
+  };
+  carrinho.push(novoProduto);
+  localStorage.setItem('carrinho', JSON.stringify(carrinho));
+}
