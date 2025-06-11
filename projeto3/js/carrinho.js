55a9a32644e76e40abee1daf0823eb34d1967de2
arthurpiloto10 <arthursccp76@gmail.com>
2025-06-11 18:22:17 -0300

    Resolução de bugs e adição do calculo do total dos produtos.

--- projeto3/js/carrinho.js
+++ projeto3/js/carrinho.js
@@ -9,32 +9,47 @@ document.addEventListener("DOMContentLoaded", () => {
   let html = "";
   carrinho.forEach((produto) => {
     html += `
-    <tr>
+    <tr id="linha-produto-${produto.id}">
                     <td class="d-flex align-items-center">
-                        <img src="./img/${produto.imagem}" alt="${produto.nome}" style="width: 75px;">
+                        <img src="./img/${produto.imagem}" alt="${
+      produto.nome
+    }" style="width: 75px;">
                         <span>
                             ${produto.nome}
                         </span>
                     </td>
                     <td class="text-center">
-                        <button class="btn" onclick="diminuiQuantidadeDoProdutoNoCarrinho(${produto.id})">
+                        <button class="btn" onclick="diminuiQuantidadeDoProdutoNoCarrinho(${
+                          produto.id
+                        })">
                             <i class="fa-solid fa-minus"></i>
                         </button>
-                        <button class="btn">
+                        <button class="btn" id="quantidade-produto-${
+                          produto.id
+                        }">
                             ${produto.quantidade}
                         </button>
-                        <button class="btn" onclick="aumentaQuantidadeDoProdutoNoCarrinho(${produto.id})">
+                        <button class="btn" onclick="aumentaQuantidadeDoProdutoNoCarrinho(${
+                          produto.id
+                        })">
                             <i class="fa-solid fa-plus"></i>
                         </button>
                     </td>
                     <td class="text-center">
-                        R$ ${produto.preco.toFixed(2).replace('.', ',')}
+                        R$ ${produto.preco.toFixed(2).replace(".", ",")}
                     </td>
                     <td class="text-center">
-                        R$ ${produto.preco.toFixed(2).replace('.', ',')}
+                        R$ 
+                        <span id="total-produto-${produto.id}">
+                        ${(produto.preco * produto.quantidade)
+                          .toFixed(2)
+                          .replace(".", ",")}
+                          </span>
                     </td>
                     <td class="text-center">
-                        <button class="btn btn-warning" onclick="removerProdutoDoCarrinho(${produto.id})">
+                        <button class="btn btn-warning" onclick="removerProdutoDoCarrinho(${
+                          produto.id
+                        })">
                             <i class="fa-solid fa-xmark"></i>
                         </button>
                     </td>
@@ -44,33 +59,66 @@ document.addEventListener("DOMContentLoaded", () => {
   corpoCarrinho.innerHTML = html;
 });
 
-
 function aumentaQuantidadeDoProdutoNoCarrinho(id) {
-    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
-    let produto = carrinho.find(produto => produto.id === id);
-    if (produto) {
-        produto.quantidade += 1;
-        localStorage.setItem('carrinho', JSON.stringify(carrinho));
-    }
-    location.reload();
+  let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+  let produto = carrinho.find((produto) => produto.id === id);
+  if (produto) {
+    produto.quantidade += 1;
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+  }
+  //location.reload();
+  atualizaTotalDoProdutoNaLinhaDaTabela(id);
+  atualizaQuantidadeDoProdutoNaLinhaDaTabela(id);
+  atualizarContadorDeProdutosNoCarrinho();
 }
 
 function diminuiQuantidadeDoProdutoNoCarrinho(id) {
-    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
-    let produto = carrinho.find(produto => produto.id === id);
-    if (produto && produto.quantidade > 1) {
-        produto.quantidade -= 1;
-        localStorage.setItem('carrinho', JSON.stringify(carrinho));
-    }
-    location.reload();
+  let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+  let produto = carrinho.find((produto) => produto.id === id);
+  if (produto && produto.quantidade > 1) {
+    produto.quantidade -= 1;
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+  }
+  //location.reload();
+  atualizaTotalDoProdutoNaLinhaDaTabela(id);
+  atualizaQuantidadeDoProdutoNaLinhaDaTabela(id);
+  atualizarContadorDeProdutosNoCarrinho();
+}
+
+function atualizaTotalDoProdutoNaLinhaDaTabela(id) {
+  let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+  let produto = carrinho.find((produto) => produto.id === id);
+  document.getElementById(`total-produto-${id}`).textContent = (produto.preco * produto.quantidade).toFixed(2).replace('.' , ',');
+}
+
+function quantidadeDoProdutoNoCarrinho(id) {
+  let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+  let produto = carrinho.find((produto) => produto.id === id);
+  return produto.quantidade;
+}
+
+function atualizaQuantidadeDoProdutoNaLinhaDaTabela(id) {
+  document.getElementById(`quantidade-produto-${id}`).textContent =
+    quantidadeDoProdutoNoCarrinho(id);
 }
 
 function removerProdutoDoCarrinho(id) {
-    let confimacao = window.confirm("Deseja realmente excluir o produto do carrinho?");
-    if (confimacao) {
-        let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
-        carrinho = carrinho.filter(produto => produto.id !== id);
-        localStorage.setItem('carrinho', JSON.stringify(carrinho));
-        location.reload();
+  let confimacao = window.confirm(
+    "Deseja realmente excluir o produto do carrinho?"
+  );
+  if (confimacao) {
+    let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+    carrinho = carrinho.filter((produto) => produto.id !== id);
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+    //location.reload();
+    let linha = document.getElementById(`linha-produto-${id}`);
+    if (linha) {
+      linha.remove();
+      atualizarContadorDeProdutosNoCarrinho();
     }
+    if (carrinho.length === 0) {
+      document.getElementById("corpo-carrinho").innerHTML =
+        '<tr><td class="text-center" colspan="5">Carrinho Vazio</td></tr>';
+    }
+  }
 }
--- projeto3/js/home.js
+++ projeto3/js/home.js
@@ -39,14 +39,19 @@ window
 
 function adicionarProdutoAoCarrinho(p_id, p_nome, p_imagem, p_preco) {
   let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
-  let novoProduto = {
-    id: p_id,
-    nome: p_nome,
-    imagem: p_imagem,
-    preco: p_preco,
-    quantidade: 1,
-  };
-  carrinho.push(novoProduto);
+  let produtoExistente = carrinho.find((produto) => produto.id === p_id);
+  if (produtoExistente) {
+    produtoExistente.quantidade += 1;
+  } else {
+    let novoProduto = {
+      id: p_id,
+      nome: p_nome,
+      imagem: p_imagem,
+      preco: p_preco,
+      quantidade: 1,
+    };
+    carrinho.push(novoProduto);
+  }
   localStorage.setItem("carrinho", JSON.stringify(carrinho));
   atualizarContadorDeProdutosNoCarrinho();
 }
