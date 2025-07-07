cd805e58eab847bf554e58c250716be9b831887e
arthurpiloto10 <arthursccp76@gmail.com>
2025-07-07 18:25:36 -0300

    Ajuste na página de Checkout e Mensagem para o WhatsApp

--- projeto3/js/checkout.js
+++ projeto3/js/checkout.js
@@ -10,4 +10,27 @@ document
       alert("Seu carrinho está vazio!");
       return;
     }
+    let mensagem = `*Novo pedido recebido!*\n\n`;
+    mensagem += `*Nome:* ${nome}\n`;
+    mensagem += `*Email:* ${email}\n`;
+    mensagem += `*Telefone:* ${telefone}\n\n`;
+    mensagem += `*Produtos selecionados:*`;
+    let total = 0;
+    carrinho.forEach((produto, index) => {
+      const subtotal = produto.preco * produto.quantidade;
+      total += subtotal;
+      mensagem += `\n${index + 1}. ${produto.nome} - ${
+        produto.quantidade
+      } X R$ ${produto.preco.toFixed(2).replace(".", ",")} = R$ ${subtotal
+        .toFixed(2)
+        .replace(".", ",")}`;
+    });
+    mensagem += `\n\n*Total:* R$ ${total.toFixed(2).replace('.' , ',')}`;
+    const mensagemCodificada = encodeURIComponent(mensagem);
+    const numeroWhatsapp = "+5517991005637";
+    // window.location.href = `https://wa.me/${numeroWhatsapp}?text=${mensagemCodificada}`;
+    window.open(`https://wa.me/${numeroWhatsapp}?text=${mensagemCodificada}`, '_blank');
+    localStorage.removeItem('carrinho');  
+    this.reset();
+    atualizarContadorDeProdutosNoCarrinho();
   });
