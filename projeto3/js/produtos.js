812a79897d184606c25e4482de2bf2349257f3ad
arthurpiloto10 <arthursccp76@gmail.com>
2025-07-30 19:01:42 -0300

    Criação da página de produtos

---
+++ projeto3/js/produtos.js
@@ -0,0 +1,57 @@
+window
+  .fetch("http://127.0.0.1:5500/projeto3/js/produtos.json")
+  .then((resposta) => resposta.json())
+  .then((produtos) => {
+    const listaProdutos = document.getElementById("lista-produtos");
+    let html = ``;
+    produtos.forEach((produto) => {
+      html += `
+        <div class="card p-2 w-lg-25">
+                <div class="imagem d-flex align-items-center justify-content-center">
+                    <img src="./img/${produto.imagem}" alt="${
+        produto.nome
+      }" class="img-fluid">
+                </div>
+                <h3 class="text-uppercase mt-1">
+                    ${produto.nome}
+                </h3>
+                <p class="mb-1 mt-1">
+                    ${produto.descricao}
+                </p>
+                <div class="d-flex justify-content-between">
+                    <span>
+                        R$
+                        <span class="text-success">
+                            ${produto.preco.toFixed(2).replace(".", ",")}
+                        </span>
+                    </span>
+                    <a onclick="adicionarProdutoAoCarrinho(${produto.id}, '${
+        produto.nome
+      }', '${produto.imagem}', ${produto.preco})" class="btn1">
+                        <i class="fa-solid fa-plus"></i>
+                    </a>
+                </div>
+            </div>
+        `;
+    });
+    listaProdutos.innerHTML = html;
+  });
+
+function adicionarProdutoAoCarrinho(p_id, p_nome, p_imagem, p_preco) {
+  let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+  let produtoExistente = carrinho.find((produto) => produto.id === p_id);
+  if (produtoExistente) {
+    produtoExistente.quantidade += 1;
+  } else {
+    let novoProduto = {
+      id: p_id,
+      nome: p_nome,
+      imagem: p_imagem,
+      preco: p_preco,
+      quantidade: 1,
+    };
+    carrinho.push(novoProduto);
+  }
+  localStorage.setItem("carrinho", JSON.stringify(carrinho));
+  atualizarContadorDeProdutosNoCarrinho();
+}
